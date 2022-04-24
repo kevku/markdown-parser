5ccabe95bc5e2e2646711a6f2f8dd5f6c915f871
kevk <kevinku668833@gmail.com>
2022-04-24 16:42:54 -0700

    Moved Break Statement
    Break was moved higher so it can prevent the first link from printing again.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,29 +12,20 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            try {
-                int openBracket = markdown.indexOf("[", currentIndex);
-                int openParen = markdown.indexOf("(", currentIndex);
-                int closeParen = markdown.indexOf(")", openParen);
-                if (openBracket == -1) {
-                    break;
-                }
-                if (closeParen== -1) {
-                    break;
-                }
-                if (openParen== -1) {
-                    break;
-                }
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
-            } catch (IndexOutOfBoundsException e) {
-                return toReturn;
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            if (openBracket == -1) {
+                break;
             }
-            //int closeBracket = markdown.indexOf("]", openBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
 
         return toReturn;
     }
+
 
 
     public static void main(String[] args) throws IOException {
